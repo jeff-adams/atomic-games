efdbccf24776194527b814ed3b3fed53a70a6695
Jeff Adams <jeff.c.adams@outlook.com>
2021-02-14 20:14:26 -0500

    Add spritebatching and rendertarget drawing

--- Engine/Graphics/Display.cs
+++ Engine/Graphics/Display.cs
@@ -1,23 +1,71 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace AtomicGames.Graphics
 {
-    public class Display
+    public class Display : IDisposable
     {
         private Game game;
+        private SpriteBatch spriteBatch;
+        private RenderTarget2D target;
+        private bool targetIsSet = false;
         
         private float prefferedAspectRatio = 1f;
         private float currentAspectRatio = 1f;
-        public Display(Game game)
+        private float windowScale;
+        private Rectangle renderRect;
+
+        public Display(Game game, int width, int height)
         {
             this.game = game;
+            target = new RenderTarget2D(game.GraphicsDevice, width, height);
+            game.Window.ClientSizeChanged += new EventHandler<EventArgs>(UpdateScreenSize);
+            UpdateScreenSize(null, null);
+            spriteBatch = new SpriteBatch(game.GraphicsDevice);
         }
+
         public void UpdateScreenSize(object sender, EventArgs e)
         {
-            float currentAspectRatio = game.window.ClientBounds.Width / (float) game.window.ClientBounds.Height;
+            float currentAspectRatio = game.Window.ClientBounds.Width / (float) game.Window.ClientBounds.Height;
             windowScale = 1f / currentAspectRatio;
             renderRect = GetRenderTargetPositionAsRectangle(currentAspectRatio);
+        }
+
+        public void Set()
+        {
+            if (!targetIsSet)
+            {
+                game.GraphicsDevice.SetRenderTarget(target);
+                targetIsSet = true;
+            }
+        }
+
+        public void UnSet()
+        {
+            if (targetIsSet)
+            {
+                game.GraphicsDevice.SetRenderTarget(null);
+                targetIsSet = false;
+            }
+        }
+
+        public void BatchSprites(Texture2D sprite)
+        {
+            float scale = 2f;
+
+            game.GraphicsDevice.Clear(Color.PapayaWhip);
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(sprite, new Vector2(renderRect.Center.X - sprite.Width * scale * windowScale / 2, renderRect.Center.Y - sprite.Height * scale * windowScale / 2), null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+            spriteBatch.End();
+        }
+
+        public void Draw()
+        {
+            game.GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            spriteBatch.Draw(target, renderRect, Color.White);
+            spriteBatch.End();
         }
 
         private Rectangle GetRenderTargetPositionAsRectangle(float currentAspectRatio)
@@ -25,17 +73,22 @@ namespace AtomicGames.Graphics
             if (currentAspectRatio <= prefferedAspectRatio)
             {
                 // output is taller than it is wider, bars on top/bottom
-                int presentHeight = (int)((game.window.ClientBounds.Width / prefferedAspectRatio) + 0.5f);
-                int barHeight = (game.window.ClientBounds.Height - presentHeight) / 2;
-                return new Rectangle(0, barHeight, game.window.ClientBounds.Width, presentHeight);
+                int presentHeight = (int)((game.Window.ClientBounds.Width / prefferedAspectRatio) + 0.5f);
+                int barHeight = (game.Window.ClientBounds.Height - presentHeight) / 2;
+                return new Rectangle(0, barHeight, game.Window.ClientBounds.Width, presentHeight);
             }
             else
             {
                 // output is wider than it is tall, bars left/right
-                int presentWidth = (int)((game.window.ClientBounds.Height * prefferedAspectRatio) + 0.5f);
-                int barWidth = (game.window.ClientBounds.Width - presentWidth) / 2;
-                return new Rectangle(barWidth, 0, presentWidth, game.window.ClientBounds.Height);
+                int presentWidth = (int)((game.Window.ClientBounds.Height * prefferedAspectRatio) + 0.5f);
+                int barWidth = (game.Window.ClientBounds.Width - presentWidth) / 2;
+                return new Rectangle(barWidth, 0, presentWidth, game.Window.ClientBounds.Height);
             }
+        }
+
+        public void Dispose()
+        {
+            game.Window.ClientSizeChanged -= UpdateScreenSize;
         }
     }
 }
---
+++ Sample/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using AtomicGames.Graphics;
+
+namespace Sample
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private Display display;
+
+        private Texture2D tree;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
+            Window.AllowUserResizing = true;
+
+            display = new Display(this, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // TODO: use this.Content to load your game content here
+            tree = Content.Load<Texture2D>("BigTree");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            display.Set();
+            display.BatchSprites(tree);
+            display.UnSet();
+            display.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
