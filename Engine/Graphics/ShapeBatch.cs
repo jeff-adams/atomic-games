ad5225e329911972a794d0765ff2e216d6b6c511
Jeff Adams <jeff.c.adams@outlook.com>
2024-04-04 20:55:06 -0400

    Fix the namespace, whoops!

--- Engine/Graphics/ShapeBatch.cs
+++ Engine/Graphics/ShapeBatch.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace AtomicGame.Engine.Graphics;
+namespace AtomicGames.Engine.Graphics;
 
 public sealed class ShapeBatch : IDisposable
 {
